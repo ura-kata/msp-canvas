212bb094e57758d937eba8f2788ec9ad468ea275
uttne <uttne.mkr@gmail.com>
2023-03-27 01:16:56 +0900

    描画系の関数を別のファイルに切り出し

--- src/parts/Content.tsx
+++ src/parts/Content.tsx
@@ -5,6 +5,7 @@ import { useAppContext, PultD3Data } from "../contexts/AppContext";
 import { BackgroundImageData, useBackgroundImage } from "../hooks/useBackgroundImage";
 import { usePults } from "../hooks/usePults";
 import { Menu, MenuItem } from "@mui/material";
+import { createSvg, drawBackgroundImage, drawPult, initRootSvg, setPlutShapeParam } from "../libs/canvas";
 
 export interface ContentProps {}
 
@@ -67,24 +68,6 @@ interface RootSvgData {
     contentRootRef: React.RefObject<HTMLDivElement>;
 }
 
-function createSvg(parentTagSelector: string):d3.Selection<SVGSVGElement, unknown, HTMLElement, any> {
-    const s = d3
-            .select(parentTagSelector)
-            .append("svg")
-        .attr("class", "svg-canvas");
-    return s;
-}
-
-function initRootSvg(svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>) {
-    if (!svg)
-        return;
-    const s = svg;
-    s.append("g").attr("class", "background-layer");
-    s.append("g").attr("class", "draw-layer");
-    const viewbox = `0 0 0 0`; 
-    s.attr("viewBox", viewbox);
-}
-
 function useRootSvg(): RootSvgData {
     const svg = useRef(
         null as d3.Selection<SVGSVGElement, unknown, HTMLElement, any> | null
@@ -117,136 +100,6 @@ function useRootSvg(): RootSvgData {
 
 const DEBUG_SHAPE_RELOAD = 9;
 
-function setPlutShapeParam(
-    circle: d3.Selection<SVGCircleElement, PultD3Data, d3.BaseType, unknown>,
-    text: d3.Selection<SVGTextElement, PultD3Data, d3.BaseType, unknown>
-) {
-    circle
-        .attr("cx", "100")
-        .attr("cy", "100")
-        .attr("r", (d) => 100)
-        .attr("fill", (d: any) => d.color ? d.color : "#000")
-        .attr("class", (d) => "plut-drag-" + d.id);
-
-    text.attr("x", "50%")
-        .attr("y", "50%")
-        .attr("text-anchor", "middle")
-        .attr("dominant-baseline", "central")
-        .attr("class", (d) => "plut-drag-" + d.id)
-        .attr("font-size", "80")
-        .attr("fill", "#fff")
-        .text((d) => d.display);
-}
-
-function drawBackgroundImage(
-    svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>,
-    background?: BackgroundImageData,
-    isEmbedded: boolean = false
-) {
-    const width = background?.width ?? 0;
-    const height = background?.height ?? 0;
-    const viewbox = `0 0 ${width} ${height}`; 
-    svg.attr("viewBox", viewbox);
-
-    const chain = svg
-        .select(".background-layer")
-        .selectAll<SVGImageElement, unknown>("image")
-        .data([background]);
-
-    chain.exit().remove();
-    const chainUpdate = chain.enter().append("image").merge(chain);
-
-    chainUpdate
-        .attr("x", "0")
-        .attr("y", "0")
-        .attr("href", (d) => {
-            if (isEmbedded) {
-                return d?.embeddedUrl ?? "";
-            }
-            return d?.url ?? ""; 
-        } );
-}
-
-function drawPult(
-    svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>,
-    data: PultD3Data[],
-    handleContextMenu: (clientX: number, clientY: number, d: PultD3Data) => void
-) {
-    const layer = svg.select(".draw-layer");
-    {
-        const move = (d: PultD3Data) => {
-            const data = [d];
-            layer
-                .select<SVGSVGElement>(".plut-g.plut-drag-" + d.id)
-                .data(data)
-                .attr("x", (d) => d.cx - 100)
-                .attr("y", (d) => d.cy - 100);
-        };
-
-        const dragStarted = (e: any, d: PultD3Data) => {};
-
-        const dragged = (e: any, d: PultD3Data) => {
-            const dx = e.dx;
-            const dy = e.dy;
-
-            d.cx += dx;
-            d.cy += dy;
-            move(d);
-        };
-        const dragEnded = (e: any, d: PultD3Data) => {};
-        const dragSvg = d3
-            .drag<SVGSVGElement, PultD3Data>()
-            .on("start", dragStarted)
-            .on("drag", dragged)
-            .on("end", dragEnded);
-
-        const handleOnContextMenu = (e: PointerEvent, d: PultD3Data) => {
-            // プルトを文字で入力できるようにしたのでコンテキストメニューで削除をとりあえずしないようにする
-            // TODO : ただ pultText から削除する処理を書けばできるのでそれも検討しておく
-            e.preventDefault();
-
-            // e の client のポジションが div と一致しているのでとりあえずこれをそのまま使う
-            handleContextMenu(e.clientX, e.clientY, d);
-        };
-
-        const chain = layer
-            .selectAll<SVGSVGElement, unknown>(".plut-g")
-            .data(data);
-
-        // exit の後は多い分のデータの処理なので remove で削除する
-        chain.exit().remove();
-
-        // enter の後は増えた分のデータの処理なので g を足しておく
-        const newChainSvg = chain.enter().append("svg");
-
-        // どんなターゲットでも共通の処理を初期化として与えておく
-        newChainSvg.append("circle");
-        // .on("contextmenu", handleOnContextMenu);
-        newChainSvg.append("text");
-        // .on("contextmenu", handleOnContextMenu);
-
-        // 増えた分に merge で update 分(通常の select の後)を足して一緒に処理をする
-        // type が変わったときにも更新できるように
-        newChainSvg
-            .merge(chain)
-            .attr("class", (d) => "plut-g plut-drag-" + d.id)
-            .attr("viewBox", "0 0 200 200")
-            .attr("height", "200")
-            .attr("width", "200")
-            .attr("x", (d) => d.cx - 100)
-            .attr("y", (d) => d.cy - 100)
-            .call(dragSvg);
-
-        // データが変わったときに必ず全体を更新する
-        const circle = layer
-            .selectAll<SVGCircleElement, unknown>(".plut-g > circle")
-            .data(data);
-        const text = layer
-            .selectAll<SVGTextElement, unknown>(".plut-g > text")
-            .data(data);
-        setPlutShapeParam(circle, text);
-    }
-}
 
 export function Content(props: ContentProps) {
     const { data, setData } = useAppContext();
